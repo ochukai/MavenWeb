53164f7cb3a55246c522a41bf74ec859ecd502e7
xiaoyee <2212lx@163.com>
2013-04-14 23:45:58 +0800

    解决冲突

---
+++ src/main/java/com/onakaumi/model/User.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.onakaumi.model;
+
+/**
+ * @author Onakaumi
+ * @since 2013-4-14 下午11:38:43
+ *
+ */
+public class User {
+
+}
---
+++ src/main/java/com/onakaumi/web/action/UserAction.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.onakaumi.web.action;
+
+/**
+ * @author Onakaumi
+ * @since 2013-4-14 下午11:38:55
+ *
+ */
+public class UserAction {
+
+}
